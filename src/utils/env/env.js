cc9c4a6a8cfb678a343ecc1b9273759753abad01
justanothergamecompany <justanothergamecompanyc@gmail.com>
2023-06-18 13:49:18 -0300

    change envs to production env

--- src/utils/env/env.js
+++ src/utils/env/env.js
@@ -1,4 +1,4 @@
-export const VITE_PROXY = import.meta.env.VITE_PROXY;
-export const VITE_PROXY_KEY = import.meta.env.VITE_PROXY_KEY;
-export const VITE_DROPMAIL_API_ENDPOINT = import.meta.env.VITE_DROPMAIL_API_ENDPOINT;
-export const VITE_DROPMAIL_API_TOKEN = import.meta.env.VITE_DROPMAIL_API_TOKEN;
+export const VITE_PROXY = process.env.VITE_PROXY;
+export const VITE_PROXY_KEY = process.env.VITE_PROXY_KEY;
+export const VITE_DROPMAIL_API_ENDPOINT = process.env.VITE_DROPMAIL_API_ENDPOINT;
+export const VITE_DROPMAIL_API_TOKEN = process.env.VITE_DROPMAIL_API_TOKEN;
