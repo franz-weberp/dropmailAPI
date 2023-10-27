a69286d2fd318a2160ba211df5734fc7440e9b87
franz-weberp <fweber.pimentel@gmail.com>
2023-10-27 10:56:12 -0300

    temp fix enable cors

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <div class="container p-0 d-flex justify-content-end"><CButtonColorMode/></div>
+    <div class="container p-0 d-flex justify-content-end">
+      <div class="flex-wrap d-flex align-content-center">After enable the cors Refresh the page</div>
+      <CButtonEnableCors/>
+      <CButtonColorMode/>
+    </div>
     <HomeView />
   </div>
 </template>
 
 <script>
+import CButtonEnableCors from '@components/C-ButtonEnableCors.vue'
 import CButtonColorMode from '@components/C-ButtonColorMode.vue'
 import HomeView from './views/HomeView.vue'
 
 export default {
 	components: {
+    CButtonEnableCors,
     CButtonColorMode,
 		HomeView,
   },
---
+++ src/components/C-ButtonEnableCors.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="openHerokuCors" class="btn btn-primary mx-2">Enable Cors</button>
+</template>
+
+<script>
+export default {
+  data() { return { path: 'https://cors-anywhere.herokuapp.com' }},
+
+  methods: {
+    openHerokuCors() {
+      window.open(this.path, '_blank', 'noreferrer');
+    },
+  }
+}
+</script>
